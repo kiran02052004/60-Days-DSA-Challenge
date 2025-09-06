d325a0116e2a6c95c0e7bd549b199fc0bb9ef4fb
Bhumika2101 <bhumikasalunkhe283@gmail.com>
2025-09-06 19:35:28 +0530

    second day problems solution

---
+++ Day_2/Java/greatestOfThree.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class greatestOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a >= b && a >= c) {
+            System.out.println("Greatest number is: " + a);
+        } else if (b >= a && b >= c) {
+            System.out.println("Greatest number is: " + b);
+        } else {
+            System.out.println("Greatest number is: " + c);
+        }
+    }
+
+}
---
+++ Day_2/Java/secondLargest.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class secondLargest {
+    static int findSecondLargest(int[] arr, int n) {
+        int first = arr[0];
+        int second = arr[0];
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > first) {
+                second = first;
+                first = arr[i];
+            } else if (arr[i] > second && arr[i] != first) {
+                second = arr[i];
+            }
+        }
+
+        return second;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(findSecondLargest(arr, n));
+
+    }
+
+}
