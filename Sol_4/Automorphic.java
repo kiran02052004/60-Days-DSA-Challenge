af7dc2e662ced6060e4843e0d70ec8c71aee48cc
Kiran Patil <kiranpatil452004@gmail.com>
2025-09-13 22:34:26 +0530

    added Solution 3

---
+++ Sol_4/Automorphic.java
@@ -0,0 +1,14 @@
+package Sol_4;
+
+public class Automorphic {
+    public static void main(String[] args) {
+        int n =25;
+        
+        if(square ==n){
+            System.out.println("Automorphic");
+        }
+        else{
+            System.out.println("Not Automorphic");
+        }
+    }
+}
